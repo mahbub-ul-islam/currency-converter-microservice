9a628ffe68a80c61df23b84b1561121a9411ff2e
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2023-04-30 12:35:15 +0600

    limit-service: adding Configuration to Get Value From Properties For Get Request. closes #3

--- limit-service/src/main/java/com/learning/limitservice/controller/LimitsController.java
+++ limit-service/src/main/java/com/learning/limitservice/controller/LimitsController.java
@@ -1,6 +1,8 @@
 package com.learning.limitservice.controller;
 
+import com.learning.limitservice.config.Configaration;
 import com.learning.limitservice.model.Limits;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class LimitsController {
 
+    @Autowired
+    private Configaration configaration;
+
     @GetMapping("/limits")
     public Limits retriveLimits() {
-        return new Limits(1, 1000);
+//        return new Limits(1, 1000);
+        return new Limits(configaration.getMinimum(), configaration.getMaximum());
     }
 }
