eeef66a9618c00c8140b7a74534b9ca19940ef2e
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2023-04-30 19:28:06 +0600

    currency-exchange: adding jpa and sample data. closes #11
    #    for hibernate to create first before executing data.sql
    spring.jpa.defer-datasource-initialization=true

--- currency-exchange-service/src/main/java/com/learning/currencyexchangeservice/model/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/learning/currencyexchangeservice/model/CurrencyExchange.java
@@ -5,17 +5,30 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
 import java.math.BigDecimal;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
 public class CurrencyExchange {
 
+    @Id
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
+
     private BigDecimal conversionMultiple;
+
+    @Transient
     private String environment;
 
     public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
