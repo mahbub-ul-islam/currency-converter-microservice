bcea5be4e57505c4f5871716bb8858573780ced5
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2023-05-01 20:42:59 +0600

    currency-exchange: adding rate limit. closes #24

--- currency-exchange-service/src/main/java/com/learning/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/learning/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,6 +1,7 @@
 package com.learning.currencyexchangeservice.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -14,17 +15,21 @@ public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
 //    @Retry(name = "sample-api", fallbackMethod = "fallback")
-    @CircuitBreaker(name = "default", fallbackMethod = "fallback")
+//    @CircuitBreaker(name = "default", fallbackMethod = "fallback")
+//    RateLimiter => in 10s 10000 calls to the api
+    @RateLimiter(name = "default")
     public String sampleApi() {
         log.info(" ----> Sample api call received.");
 
-        ResponseEntity<String> entity =
-                new RestTemplate().getForEntity(
-                        "http://localhost:8080/some-dummy-url",
-                        String.class
-                );
+//        ResponseEntity<String> entity =
+//                new RestTemplate().getForEntity(
+//                        "http://localhost:8080/some-dummy-url",
+//                        String.class
+//                );
 
-        return entity.getBody();
+//        return entity.getBody();
+
+        return "sample-api";
     }
 
     public String fallback(Exception ex) {
