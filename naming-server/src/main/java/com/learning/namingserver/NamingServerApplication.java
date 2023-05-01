8078abc42df98957b5c152415afab53528134773
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2023-05-01 12:29:20 +0600

    naming-server: adding configuration for Initial Project. closes #17

--- naming-server/src/main/java/com/learning/namingserver/NamingServerApplication.java
+++ naming-server/src/main/java/com/learning/namingserver/NamingServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class NamingServerApplication {
 
     public static void main(String[] args) {
