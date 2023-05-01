655d75a93e8d37e966d216d498b4db75401f6038
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2023-05-01 15:58:38 +0600

    naming-server: Create Multiple Instance and load balance. closes #19
    To create Multiple instance
    
    Goto > Run > Edit Configurations
    select first instance then click Modify Options and check Add VM Options
    on the VM Options Field add
    -Dserver.port=8000
    
    then create another instance using duplicate button and same process
    -Dserver.port=8001

--- currency-conversion-service/src/main/java/com/learning/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/learning/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.math.BigDecimal;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
