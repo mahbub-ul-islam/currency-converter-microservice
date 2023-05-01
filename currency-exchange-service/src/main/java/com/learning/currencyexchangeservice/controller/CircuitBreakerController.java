cc11660e0bfc9efbfc7549038e16a86d46afa575
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2023-05-01 19:14:52 +0600

    currency-exchange: adding dummy controller which will break. closes #24

--- currency-exchange-service/src/main/java/com/learning/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/learning/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,13 +1,25 @@
 package com.learning.currencyexchangeservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @RestController
 public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
     public String sampleApi() {
-        return "Sample API";
+        log.info(" ----> Sample api call received.");
+
+        ResponseEntity<String> entity =
+                new RestTemplate().getForEntity(
+                        "http://localhost:8080/some-dummy-url",
+                        String.class
+                );
+
+        return entity.getBody();
     }
 }
