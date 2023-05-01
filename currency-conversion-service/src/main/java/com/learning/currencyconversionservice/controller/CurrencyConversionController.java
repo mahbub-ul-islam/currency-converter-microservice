6afa9efcd1ce6bdcabaf4172a34f8251c8602a8c
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2023-05-01 10:31:44 +0600

    currency-conversion: adding sample controller and model. closes #14

---
+++ currency-conversion-service/src/main/java/com/learning/currencyconversionservice/controller/CurrencyConversionController.java
@@ -0,0 +1,23 @@
+package com.learning.currencyconversionservice.controller;
+
+import com.learning.currencyconversionservice.model.CurrencyConversion;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/currency-conversion")
+public class CurrencyConversionController {
+
+    @GetMapping("/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ) {
+        return new CurrencyConversion(1L, from, to, BigDecimal.ONE, quantity, BigDecimal.ONE, "");
+    }
+}
